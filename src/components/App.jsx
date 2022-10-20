b6241048fd9d5635f07f35dbf886260c0aff1379
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-20 15:47:29 +0300

    add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,6 +33,17 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() { 
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) { 
+    if (this.state.contacts !== prevState.contacts) { 
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <>
