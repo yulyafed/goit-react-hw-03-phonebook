fd7989fc4185e04e0f54f0891714020ed383247b
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-23 14:40:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export class App extends Component {
     filter: '',
   };
 
-    filteredContacts = () => {
+  filteredContacts = () => {
     if (this.state.filter.trim() === '') {
       return this.state.contacts;
     }
